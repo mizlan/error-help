1f893ebfb616c88e74d74d0f094b825d88deea40
Michael Lan <michaellan202@gmail.com>
2021-03-15 15:18:55 -0700

    Get compiler output in JSON format

--- src/error_help/collect.clj
+++ src/error_help/collect.clj
@@ -1,2 +1,13 @@
-(ns error-help.collect)
+(ns error-help.collect
+  (:require [clojure.java.shell :as shell]))
 
+(def default-compiler-executable "g++-10")
+
+(defn compilation-output
+  "Get the compilation output using a compiler executable (must be in $PATH, has a definable default value) and a filepath"
+  ([filepath] (compilation-output default-compiler-executable filepath))
+  ([executable filepath] (shell/sh
+                          executable
+                          "-Wall"
+                          "-fdiagnostics-format=json"
+                          filepath)))
