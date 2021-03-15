74bac8f35d14bcb1dd622003e37be20d83bb83e1
Michael Lan <michaellan202@gmail.com>
2021-03-15 14:37:18 -0700

    Track prune.clj

---
+++ src/error_help/prune.clj
@@ -0,0 +1,8 @@
+(ns error-help.prune
+  (:require [clojure.data.json :as json]))
+
+(defn prune-errors
+  "Takes a JSON list and prunes out the
+  irrelevant errors/messages. Returns a
+  non-nested list"
+  [xs])
