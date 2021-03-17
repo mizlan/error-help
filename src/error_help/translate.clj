a8ac5e1cb1f650442fb2c51201be0e877e9597e6
Michael Lan <michaellan202@gmail.com>
2021-03-16 17:47:23 -0700

    helper functions

--- src/error_help/translate.clj
+++ src/error_help/translate.clj
@@ -4,3 +4,31 @@
   (:require [clansi :refer [style]]
             [jansi-clj.core :refer :all]
             [clojure.string :as str]))
+
+;; Thanks to https://github.com/AvisoNovate/pretty/blob/master/src/io/aviso/ansi.clj
+(def ^:const ^:private ansi-pattern (Pattern/compile "\\e\\[.*?m"))
+
+(defn ^String strip-ansi
+  "Removes ANSI codes from a string, returning just the raw text."
+  [string]
+  (str/replace string ansi-pattern ""))
+
+(defn space-separate
+  [& strs]
+  (str/join " " strs))
+
+(defn prune-cpp-namespaces
+  "Deletes leading namespace(s) as common in C++"
+  [token]
+  (str/replace token #".+::" ""))
+
+(defn code-style
+  [s]
+  (style s :blue :bold))
+
+(defn a-an
+  "Conditionally use `a` or `an`. The English language sucks."
+  [s]
+  (if (re-find #"^\s*[aeiou]" (strip-ansi s))
+    (str "an " s)
+    (str "a " s)))
